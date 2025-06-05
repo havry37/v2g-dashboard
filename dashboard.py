f747f0a8e321fc0713adc8c077c5966410a5eaa6
havry37 <ondrej.havran15@gmail.com>
2025-06-05 14:51:54 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -427,7 +427,7 @@ def extract_keywords(text_series, n_keywords=30):
     
     all_text = " ".join(text_series.tolist())
     stop_words = set(stopwords.words('english'))
-    custom_stops = {'would', 'could', 'get', 'also', 'even', 'like', 'one', 'make', 'just', 'v2g', 'ev', 'vehicle', 'battery', 'power', 'grid', 'energy', 'time', 'use', 'using', 'used', 'car', 'thing'}
+    custom_stops = {'would', 'could', 'get', 'also', 'even', 'like', 'one', 'make', 'much', 'think', 'charging', 'charge', 'way', 'see', 'sure', 'really', 'just', 'bidirectional', 'v2g', 'ev', 'vehicle', 'battery', 'power', 'energy', 'time', 'use', 'using', 'used', 'car', 'thing'}
     stop_words.update(custom_stops)
     
     words = word_tokenize(all_text.lower())
@@ -442,7 +442,7 @@ def extract_bigrams(text_series, n_bigrams=30):
     
     all_text = " ".join(text_series.tolist())
     stop_words = set(stopwords.words('english'))
-    custom_stops = {'would', 'could', 'get', 'also', 'even', 'like', 'one', 'make', 'just', 'v2g', 'ev', 'battery', 'power', 'grid', 'time', 'use', 'using', 'used', 'car', 'thing'}
+    custom_stops = {'would', 'could', 'get', 'also', 'even', 'like', 'bidirectional charging', 'directional charging', 'electric vehicles', 'bidirectional charger', 'bidirectional chargers', 'one', 'make', 'just', 'v2g', 'ev', 'battery', 'power', 'grid', 'time', 'use', 'using', 'used', 'car', 'thing'}
     stop_words.update(custom_stops)
     
     words = word_tokenize(all_text.lower())
